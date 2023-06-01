94f65094891a961fc2f52f2b16fef7e423b2a9d1
amersulieman <aksuliem@gmail.com>
2023-06-01 10:02:59 -0500

    Add a dynamic page heading

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -24,7 +24,7 @@ function GameGrid({ gameQuery }: Props) {
           lg: 3,
           xl: 5,
         }}
-        padding=" "
+        padding={2}
         spacing={3}
       >
         {isLoading &&
