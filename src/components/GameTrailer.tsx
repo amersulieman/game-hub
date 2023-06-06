21c2f5854c2d0cc5ea2d86c62d1d10b73ef3dc70
amersulieman <aksuliem@gmail.com>
2023-06-05 21:35:59 -0500

    build game trailer

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,22 @@
+import useTrailers from "../hooks/useTrailers";
+
+interface Props {
+  gameId: number;
+}
+
+function GameTrailer({ gameId }: Props) {
+  const { data, isLoading, error } = useTrailers(gameId);
+  if (isLoading) return null;
+  if (error) throw error;
+  const firstTrailer = data?.results[0];
+
+  return firstTrailer ? (
+    <video
+      src={firstTrailer.data[480]}
+      poster={firstTrailer.preview}
+      controls
+    ></video>
+  ) : null;
+}
+
+export default GameTrailer;
